2e298ff9dace28d81a3adecf2389d0b960d8db25
Jyothish <jyothishmb1992@gmail.com>
2021-10-11 18:53:21 +0530

    Domain Project Added

---
+++ src/Core/GloboTicket.TicketManagement.Domain/Entities/Category.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using GloboTicket.TicketManagement.Domain.Common;
+
+namespace GloboTicket.TicketManagement.Domain.Entities
+{
+    public class Category : AuditableEntity
+    {
+        public Guid CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection Events { get; set; }
+    }
+}
